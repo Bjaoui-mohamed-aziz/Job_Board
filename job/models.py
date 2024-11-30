c85c31d9819941fd9b35444168f27d069ca3033b
Bjaoui-mohamed-aziz <azizbjaoui312@gmail.com>
2024-11-30 12:17:38 +0100

    complete the image upload

--- job/models.py
+++ job/models.py
@@ -8,12 +8,18 @@ JOB_TYPE = (
         ('Part Time', 'Part Time'),
         ('Freelance', 'Freelance'),
     )
+def image_upload (instance, filename):
+     imagename , extension = filename.split(".")
+     return "jobs/%s/%s.%s"%(instance.id,instance.id,   extension)
+
+
 class Job(models.Model):  # table
     title = models.CharField(max_length=100)  # column
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)  # column
     description = models.TextField(max_length=1000)  # column
     published_at = models.DateTimeField(auto_now=True)  # column
     Vacancy = models.IntegerField(default=1)  # column
+    image = models.ImageField(upload_to=image_upload)  # column 
     salary = models.IntegerField(default=0)  # column
     category = models.ForeignKey('Category', on_delete=models.CASCADE)  # column
     experience = models.IntegerField(default=1)  # column
--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
