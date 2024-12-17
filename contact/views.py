2994ced514d15c2076666878d6c3ca3b16ee88d6
Bjaoui-mohamed-aziz <azizbjaoui312@gmail.com>
2024-12-17 19:15:26 +0100

    complete the send email feature

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 
+from project import settings
+
+from .models import Info
+from django.core.mail import send_mail
 # Create your views here.
 
 def send_message(request):
-    return render(request, 'contact/contact.html')
+    myinfo = Info.objects
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [email],)
+
+    return render(request, 'contact/contact.html', {'myinfo' : myinfo})
--- project/settings.py
+++ project/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'job',
     'bootstrap4',
-    'django_filters'
+    'django_filters',
+    'contact'
+
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'azizbjaoui312@gmail.com'
+EMAIL_HOST_PASSWORD = 'knfg uepj cfgu aooe'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
